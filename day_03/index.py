980e188eab606c1ba5b8e86be385557a013dea23
Andreas Ny <andreas.nyh87@gmail.com>
2024-12-10 17:03:23 +0100

    Day3 part 2

--- day_03/index.py
+++ day_03/index.py
@@ -2,10 +2,6 @@ import re
 import time
 
 
-# Save timestamp
-start = time.time()
-
-
 def multiply_arr(arr):
     res = 0
     for [a, b] in arr:
@@ -15,6 +11,8 @@ def multiply_arr(arr):
 
 
 def partOne():
+    # Save timestamp
+    start = time.time()
     # f = open("day_03/input.txt", "r")
     f = open("day_03/test_input.txt", "r")
 
@@ -33,8 +31,46 @@ def partOne():
 
     print("Part 1:", multiply_arr(to_multiply))
     f.close()
+    end = time.time()
+    print(end - start, "s\n")
 
 
-partOne()
-end = time.time()
-print(end - start, "s\n")
+def partTwo():
+    # Save timestamp
+    start = time.time()
+    f = open("day_03/test_input_2.txt", "r")
+    f = open("day_03/input.txt", "r")
+
+    if not f:
+        return
+
+    valid_muls = []
+    do = r"do\(\)"
+    dont = r"don't\(\)"
+    mul = r"mul\(\d+,\d+\)"
+    valid = True
+    for row in f:
+        for match in re.finditer(f"{do}|{dont}|{mul}", row):
+            if re.fullmatch(dont, match.group()):
+                valid = False
+            elif re.fullmatch(do, match.group()):
+                valid = True
+            elif valid:
+                valid_muls.append(match.group())
+
+    to_multiply = []
+    for mul in valid_muls:
+        to_multiply.append(re.split(",", re.search(r"\d+,\d+", mul).group()))
+
+    print("Part 2:", multiply_arr(to_multiply))
+    f.close()
+    end = time.time()
+    print(end - start, "s\n")
+
+
+def main():
+    partOne()
+    partTwo()
+
+
+main()
